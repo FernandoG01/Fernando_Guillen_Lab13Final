8e7870e9f992aa0ca6aa4175a921c2ac3e8f4d7b
fernando guillen zegarra <fguillen@tecsup.edu.pe>
2026-06-15 15:34:36 -0500

    Habilitar Swagger en producción

--- Fernando_Guillen_Lab13Final/Program.cs
+++ Fernando_Guillen_Lab13Final/Program.cs
@@ -4,25 +4,31 @@ using Fernando_Guillen_Lab13Final.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<LINQExampleDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+
 builder.Services.AddScoped<ExcelReportService>();
+
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
     options.JsonSerializerOptions.WriteIndented = true;
 });
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseHttpsRedirection();
+
 app.MapControllers();
+
 app.MapGet("/", () => "API Fernando Guillen Lab13 Final funcionando correctamente.");
 
 app.Run();
